48c6b125eb74cf64d86026b06656a67e066b290d
rtomar <rtomar@rtomar-ltp.DDN.UPES.AC.IN>
2019-02-14 13:15:13 +0530

    Sample Commit for 2019

---
+++ CCVT_Code_Distribute/src/com/bao/Class2.java
@@ -0,0 +1,10 @@
+package com.bao;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CCVT_Code_Distribute/src/com/bao/DemoClass.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello hi");
+
+	}
+
+}
